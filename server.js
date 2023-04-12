d635c89fd246eb78cfb8a12f903da504edc1a9e2
Hernan Bracamonte <carlos.bracamonte@soytul.com>
2023-04-12 17:34:53 -0500

    fix: 🐛 server.js & add dependency

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const path = require('path');
 
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 8080;
 
 const app = express();
 
-app.use(express.static('dist'));
+app.use(express.static(__dirname + '/dist'));
 
 app.get('/*', (request, response) => {
-  response.sendFile(path.resolve(__dirname, './dist', 'index.html'));
+  response.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
 app.listen(PORT, () => {
